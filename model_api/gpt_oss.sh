6460b226b9defd42a67759e14389fa0ae336b9eb
p62058974 <p62058974@example.org>
2025-12-08 15:39:29 +0800

    delete no need

--- model_api/gpt_oss.sh
+++
@@ -1,8 +0,0 @@
-python -m vllm.entrypoints.openai.api_server \
-    --model "/data/byj/hf_hub/openai/gpt_oss" \
-    --tensor-parallel-size 8 \
-    --dtype bfloat16 \
-    --trust-remote-code \
-	--gpu-memory-utilization 0.65 \
-    --host 127.0.0.1 \
-    --port 8011
